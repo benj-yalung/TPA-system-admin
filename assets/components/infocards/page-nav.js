aeec6b5bd166453c7bf1005e52d39d6baeecbf1b
benj-yalung <benjamin.yalung2020@gmail.com>
2023-10-16 17:18:54 +0800

    Add functionality to OR dashboard nav buttons

--- assets/components/infocards/page-nav.js
+++ assets/components/infocards/page-nav.js
@@ -1,6 +1,6 @@
 /**
  * This JavaScript code block selects a group of buttons within an HTML document
- * with the class "infocard-table-nav." It adds a click event listener to each
+ * with the class "page-nav." It adds a click event listener to each
  * button, such that when a button is clicked, it performs the following actions:
  *
  * - It removes the "active" class from all buttons to visually deselect them;
@@ -8,16 +8,11 @@
  * - Adds the "active" class to the clicked button to visually indicate it's selected;
  *
  * - Retrieves the value of the "data-infocard-id" attribute from the clicked button,
- * which corresponds to the ID of a target infocard; hides all dynNavItem
- with the
- * class "table-infocard" by setting their display property to "none"; and finally,
+ * which corresponds to the ID of a target infocard; hides all subNavItem with the
+ * class "sub-nav-item" by setting their display property to "none"; and finally,
  *
- * - Displays the infocard with the matching ID as a flex element, making it
+ * - Displays the item with the matching ID as a flex element, making it
  * visible.
- *
- * This code essentially controls a navigation system for dynNavItem
-,
- * ensuring that only the selected infocard is displayed while others are hidden.
  */
 
 const buttons = document.querySelectorAll(".page-nav button");
