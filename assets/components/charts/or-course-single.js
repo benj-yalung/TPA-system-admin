12067903c5ee120398530454f0958e9adc06f974
benj-yalung <benjamin.yalung2020@gmail.com>
2023-10-17 15:45:44 +0800

    Further configure chart

--- assets/components/charts/or-course-single.js
+++ assets/components/charts/or-course-single.js
@@ -5,17 +5,49 @@ const ChartOrCourseSingleSummary = document.getElementById(
 new Chart(ChartOrCourseSingleSummary, {
   type: "doughnut",
   data: {
+    labels: ["On time", "Late", "Absent"],
     datasets: [
       {
-        data: [20, 80],
+        data: [9, 5, 1],
         borderColor: "white",
-        backgroundColor: ["#718ED4", "#28EDFC"],
-        rotation: -120,
+        backgroundColor: ["#0F0346", "#28EDFC", "#A447FD"],
+        rotation: 144,
       },
     ],
   },
   options: {
     maintainAspectRatio: false,
     responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+      datalabels: {
+        labels: {
+          value: {
+            align: "end",
+            anchor: "end",
+            color: "black",
+          },
+        },
+        font: {
+          family: "roboto",
+        },
+        formatter: (value, context) => {
+          const dataLabel = context.chart.data.labels[context.dataIndex];
+
+          const datapoints = context.chart.data.datasets[0].data;
+          const sum = (total, datapoint) => total + datapoint;
+          const totalValue = datapoints.reduce(sum, 0);
+          const percentage = ((value / totalValue) * 100).toFixed(1);
+
+          return [`${dataLabel}`, `${percentage}%`];
+        },
+      },
+    },
   },
+  plugins: [ChartDataLabels],
 });
